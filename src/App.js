0aa30e66e6cbde7ea0f23feec9052798c88ad69c
AlForuq <muhammad.alforuq@gmail.com>
2023-04-25 09:34:37 +0500

    active navbar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      active: ["All plants", "Flowers", "Seeds", "Buckets"],
+      clickedPlant: null,
+    };
+  }
+
+  render() {
+    // let target = null;
+    // const onActive = (e) => {
+    //   if (target) {
+    //     target.classList.remove("active");
+    //   }
+    //   target = e.target;
+    //   target.classList.add("active");
+    // };
+
+    return (
+      <div className="div" style={{ display: "flex" }}>
+        {this.state.active.map((value) => {
+          return (
+            <h1
+              key={value}
+              onClick={() => {
+                this.setState({ clickedPlant: value });
+              }}
+              className={`link ${
+                this.state.clickedPlant === value ? "active" : ""
+              }`}
+            >
+              {value}
+            </h1>
+          );
+        })}
+      </div>
+    );
+  }
 }
 
 export default App;
